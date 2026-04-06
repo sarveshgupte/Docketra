6e32fedf7fd2ad93c3c36392648c5e42ca77c95f
guptesarvesh2611 <guptesarvesh2611@gmail.com>
2026-04-06 07:06:47 +0530

    Improve notification usability with read state and history endpoints

--- src/routes/notifications.routes.js
+++ src/routes/notifications.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { userReadLimiter } = require('../middleware/rateLimiters');
-const { getNotifications } = require('../controllers/notifications.controller');
+const {
+  getNotifications,
+  getAllNotifications,
+  markAsRead,
+} = require('../controllers/notifications.controller');
 
 const router = express.Router();
 
 router.get('/', userReadLimiter, getNotifications);
+router.get('/all', userReadLimiter, getAllNotifications);
+router.patch('/:id/read', userReadLimiter, markAsRead);
 
 module.exports = router;
