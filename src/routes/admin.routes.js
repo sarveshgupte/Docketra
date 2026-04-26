a6710fed64cd815400dcd94958f2f9bf7c5bb88e
guptesarvesh <51374351+guptesarvesh@users.noreply.github.com>
2026-04-26 22:54:05 +0530

    Restrict admin audit-log route to admin roles

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -103,7 +103,7 @@ router.patch('/categories/:id/subcategories/:subcategoryId/status', ...adminBase
 router.delete('/categories/:id/subcategories/:subcategoryId', ...adminBaseAccess, authorizeFirmPermission('CATEGORY_MANAGE'), userWriteLimiter, deleteSubcategory);
 
 router.get('/hierarchy', ...adminBaseAccess, authorizeFirmPermission('USER_VIEW'), userReadLimiter, getHierarchyTree);
-router.get('/audit-logs', ...adminBaseAccess, authorizeFirmPermission('USER_VIEW'), userReadLimiter, getAdminAuditLogs);
+router.get('/audit-logs', ...adminBaseAccess, requireManagerOrPrimaryAdmin, authorizeFirmPermission('USER_VIEW'), userReadLimiter, getAdminAuditLogs);
 router.get('/users', ...adminBaseAccess, authorizeFirmPermission('USER_VIEW'), userReadLimiter, getAllUsers);
 router.post('/users', ...adminBaseAccess, authorizeFirmPermission('USER_MANAGE'), sensitiveLimiter, createUser);
 router.put('/users/:xID/activate', ...adminBaseAccess, requirePrimaryAdmin, authorizeFirmPermission('USER_MANAGE'), sensitiveLimiter, activateUser);
